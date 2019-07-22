9fc7e0aca05f0289b18cbf4e449a11bb4f2e412e
Cynnah <massot.dorian@gmail.com>
2019-07-22 15:03:11 +0200

    installation on firebase and connect to the app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import * as firebase from 'firebase';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'bookshelves';
+  
+  constructor() {
+    var firebaseConfig = {
+      apiKey: "AIzaSyBnEYDODzceVajVtXhIwsYYlxkYmDK0FAY",
+      authDomain: "bookshelves-986c9.firebaseapp.com",
+      databaseURL: "https://bookshelves-986c9.firebaseio.com",
+      projectId: "bookshelves-986c9",
+      storageBucket: "",
+      messagingSenderId: "228635018649",
+      appId: "1:228635018649:web:7c5e6eed1aefbeb2"
+    };
+    // Initialize Firebase
+    firebase.initializeApp(firebaseConfig);
+  }
 }
